ddaa66768c382a41185030654189818e3fdc0886
Maks Novakov <maksnovakov78@gmail.com>
2022-09-09 13:45:11 +0300

    4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,19 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-const STORAGE_KEY = 'videoplayer-current-time';
-const savedTime = localStorage.getItem(STORAGE_KEY);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const STORAGE_KEY = 'videoplayer-current-time';
+// const savedTime = localStorage.getItem(STORAGE_KEY);
 
-if (savedTime) {
-  player.setCurrentTime(savedTime);
-}
+// if (savedTime) {
+//   player.setCurrentTime(savedTime);
+// }
 
-player.on('timeupdate', throttle(onPlay, 1000));
+// player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay(data) {
-  const currentTime = data.seconds;
-  localStorage.setItem(STORAGE_KEY, currentTime);
-}
+// function onPlay(data) {
+//   const currentTime = data.seconds;
+//   localStorage.setItem(STORAGE_KEY, currentTime);
+// }
+
